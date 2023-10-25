5b6f5feee1063ffe6673c56669304445b98f774c
Julie E Powell <JEPowell@proton.me>
2023-10-25 15:41:04 +1000

    format document

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,35 +1,34 @@
 import useProjects from "../../hooks/useProjects";
 import ProjectCard from "../../components/ProjectCards";
-
 import "./HomePage.css";
 
 function HomePage() {
-    const { projects, isLoading, error} = useProjects();
+    const { projects, isLoading, error } = useProjects();
 
     if (isLoading) {
         return (<p> Loading...</p>)
-      }
-    
-      if (error) {
+    }
+
+    if (error) {
         return (<p>{error.message}</p>)
-      }
-      
+    }
+
     return (
         <body>
-        <div className='hero'>
-            <img src='src/assets/reading_on_the_moon_300x300.png' alt='girl-reading-on-moon'/>
-            <h1>A crowdfunding platform for self-publishing books</h1>
-        <ol>
-            <p>1. Post your book idea for others to see. </p>
-            <p>2. Decide how much you need to accomplish your publishing goals.</p>
-            <p>3. Allow Bookarina to help you achieve your self-publishing dreams.</p>
-        </ol>    
-        </div>
-        <div className="project-list">
-            {projects.map((projectData, key) => {
-                return <ProjectCard key={key} projectData={projectData} />;
-            })}
-        </div>
+            <div className='hero'>
+                <img src='src/assets/reading_on_the_moon_300x300.png' alt='girl-reading-on-moon' />
+                <h1>A crowdfunding platform for self-publishing books</h1>
+                <ol>
+                    <p>1. Post your book idea for others to see. </p>
+                    <p>2. Decide how much you need to accomplish your publishing goals.</p>
+                    <p>3. Allow Bookarina to help you achieve your self-publishing dreams.</p>
+                </ol>
+            </div>
+            <div className="project-list">
+                {projects.map((projectData, key) => {
+                    return <ProjectCard key={key} projectData={projectData} />;
+                })}
+            </div>
         </body>
     );
 }
